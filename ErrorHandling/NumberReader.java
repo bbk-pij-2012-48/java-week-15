3fcbd0c1c85c95cd6d1aa7023d427c4c5d119ffc
Richard Bennett <rbenne06@dcs-bbk-ac-uk>
2013-01-28 21:02:07 +0000

    End of class commit - numberreader now fully functional

--- ErrorHandling/NumberReader.java
+++ ErrorHandling/NumberReader.java
@@ -1,13 +1,30 @@
 public class NumberReader {
 	public static void main(String[] args) {
-		int[] number = new int[10];
-		for (int i=0 ; i<10 ; i++) {
+		final int N = inputNumber();
+		
+		int[] number = new int[N];
+		
+		for (int i=0 ; i<N ; i++) {
+			number[i] = inputNumber();
+		}
+		for (int item : number) {
+			System.out.println(item);
+		}
+		
+	}
+	
+	private static int inputNumber() {
+		int n = 0;
+		for(;;) {
 			String tmp = System.console().readLine();
-			number[i] = Integer.parseInt(tmp);
+			try {
+				n = Integer.parseInt(tmp);
+				break;
+			} catch (NumberFormatException ex) {
+				System.err.println("Error - you did not enter an integer. Try another");
+			}
 		}
-		for (int i=0 ; i<10 ; i++) {
-			System.out.println(number[i]);
-		}
+		return n;
 	}
 }
 		
